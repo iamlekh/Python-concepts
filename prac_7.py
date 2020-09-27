0cc331da4bb26242a250ea7c1b5040d9381d12e1
iamlekh <darpanlekharu@gmail.com>
2020-09-27 16:40:07 +0530

    prac7

---
+++ prac_7.py
@@ -0,0 +1,36 @@
+from multiprocessing import Process, Value, Array
+
+
+def f(d, i, a):
+    d.value = 3.14159
+    i.value = 100
+
+    for i in range(len(a)):
+        a[i] = -a[i]
+
+
+def g(d):
+    d.value = 1.689
+
+
+if __name__ == '__main__':
+
+    double_val = Value('d', 0.0)
+    int_val = Value('i', 5)
+
+    arr_val = Array('i', range(10))
+
+    p1 = Process(target=f, args=(double_val, int_val, arr_val))
+    p1.start()
+    p1.join()
+
+    print(double_val.value)
+    print(int_val.value)
+    print(arr_val[:])
+
+    p2 = Process(target=g, args=(double_val,))
+
+    p2.start()
+    p2.join()
+
+    print(double_val.value)
